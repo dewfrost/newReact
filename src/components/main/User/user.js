45fee8a3dc7f6af28b6357a27fb7d7ffe1b653a7
dewfrost <2019396329@qq.com>
2018-04-02 10:13:34 +0800

    新项目首次提交

---
+++ src/components/main/User/user.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Model extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div className="box">
+          <header>
+          <header />
+          <div className="content">用户中心</div>
+        </header>
+      </div>
+    );
+  }
+  componentDidMount() {}
+}
+
+export default Model;
